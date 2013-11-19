7b2cedda4b82b1167472dc4f25738fe346cda203
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2013-11-19 12:14:29 +0200

    Energian tiede.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
-pdflatex -output-directory ./pruju/ pruju.tex
-pdflatex -output-directory ./laskarit/ laskarit.tex
+pdflatex -output-directory pruju/ pruju.tex
+pdflatex -output-directory laskarit/ laskarit.tex
 htlatex pruju.tex "html,mathplayer" "" "-dpruju/"
 htlatex laskarit.tex "html,mathplayer" "" "-dlaskarit/"
+cp pruju.tex pruju/
+cp laskarit.tex laskarit/
